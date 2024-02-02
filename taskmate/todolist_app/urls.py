2b489fe716c63d89f597bf67a56e10e939674764
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-02-02 01:28:21 -0600

    [ADD] Base Template and Jinja 2

--- taskmate/todolist_app/urls.py
+++ taskmate/todolist_app/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.todolist, name='todolist'),
+    path('contact', views.contact, name='contact'),
+    path('about-us', views.about, name='about'),
 ]
--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -3,6 +3,18 @@ from django.http import HttpResponse
 
 def todolist(request):
     context = {
-        'welcome_text':"Welcome To Todo List App"
+        'welcome_text':"Welcome To Todo List Page"
         }
     return render(request, 'todolist.html', context)
+
+def contact(request):
+    context = {
+        'contact_text':"Welcome To Contact Page"
+        }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        'about_text':"Welcome To About Page"
+        }
+    return render(request, 'about.html', context)

addb8ce56873043773e835a31dca70256f658918
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-02-07 14:43:36 -0600

    [ADD] Adding Form and Accepting Input

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from todolist_app.models import TaskList
+from todolist_app.forms import TaskForm
 
 def todolist(request):
-    all_tasks = TaskList.objects.all
+    if request.method == "POST":
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return redirect('todolist')
 
-    return render(request, 'todolist.html', {'all_tasks': all_tasks})
+    else:
+
+        all_tasks = TaskList.objects.all
+
+        return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 def contact(request):
     context = {

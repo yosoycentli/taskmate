12fdd0f8fa41c39e0949c69a998ff809545658e0
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-02-08 14:19:46 -0600

    [ADD] Adding Option - Mark Task as Completed

--- taskmate/todolist_app/urls.py
+++ taskmate/todolist_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.todolist, name='todolist'),
     path('delete/<task_id>', views.delete_task, name='delete_task'),
     path('edit/<task_id>', views.edit_task, name='edit_task'),
+    path('complete/<task_id>', views.complete_task, name='complete_task'),
     path('contact', views.contact, name='contact'),
     path('about-us', views.about, name='about'),
 ]

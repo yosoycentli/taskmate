514f2b67738f06209febd255385ac38bb16b4d29
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-02-08 20:22:10 -0600

    [ADD] Implementing Pagination – 1 | Pagination Logic

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 def todolist(request):
     if request.method == "POST":
@@ -14,7 +15,10 @@ def todolist(request):
 
     else:
 
-        all_tasks = TaskList.objects.all
+        all_tasks = TaskList.objects.all()
+        paginator = Paginator(all_tasks, 5)
+        page = request.GET.get('pg')
+        all_tasks = paginator.get_page(page)
 
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
     

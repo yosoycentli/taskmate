e854c6901f80300df20c8fcae5a3d3486f418887
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-02-12 14:23:11 -0600

    [ADD] Register Views

--- taskmate/users_app/views.py
+++ taskmate/users_app/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    register_form = UserCreationForm()
+    if request.method == "POST":
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, ("New User Account Created, Login To Get Started!"))
+            return redirect('register')
+    else:
+        register_form = UserCreationForm()
     return render(request, 'register.html', {'register_form': register_form})

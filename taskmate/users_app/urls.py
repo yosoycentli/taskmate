1e50138288b04c6ab4c0c60f5a456c84171129a5
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-02-12 17:26:27 -0600

    [ADD] Login - URLs, Views, and Template

--- taskmate/users_app/urls.py
+++ taskmate/users_app/urls.py
@@ -1,6 +1,9 @@
 from users_app import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register', views.register, name='register'),
+    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]

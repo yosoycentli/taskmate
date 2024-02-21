8b4253607c9ab749fd4bacc9b760b9e0ee98462b
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-02-20 22:05:43 -0600

    [FIX] CSRF Update

--- taskmate/taskmate/settings.py
+++ taskmate/taskmate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "taskmate-centli.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ['taskmate-centli.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://taskmate-centli.up.railway.app']
 
 
 # Application definition
